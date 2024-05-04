e2e22b81f63e45f288f560c4bd85a8345a481905
llssbb <bt.santosh01@gmail.com>
2024-05-04 21:25:39 +0100

    instruction code changes

--- src/llm.py
+++ src/llm.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
 from src.config import instruction
 
 load_dotenv()
@@ -9,12 +12,21 @@ load_dotenv()
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
-def ask_bot(user_message):
-    messages=[{"role": "system", "content":instruction },
-            {"role": "user", "content": user_message}]
+def ask_bot(user_message, instruction):
 
-    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
 
-    respones = llm.invoke(messages)
+
+    respones=model(
+    [
+        SystemMessage(content=instruction),
+        HumanMessage(content=user_message),
+    ]
+)
 
     return respones.content
+
+if __name__=="__main__":
+    user_message = "hi how are you?"
+    respones=ask_bot(user_message)
+    print(respones)

5e1a1ba7692e7474a8b2b2f435c306413da343e5
llssbb <bt.santosh01@gmail.com>
2024-05-04 06:27:28 +0100

    init changes

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,12 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating a conda env with python 3.9"
-conda create --prefixt ./env python=3.9 -y
+conda create --prefix ./venv python=3.9 -y
 echo [$(date)]: "created conda env"
-source activate ./env
+source activate ./venv
 echo [$(date)]: "activated conda env"
 echo [$(date)]: "installing requirements"
 pip install -r requirements.txt
 echo [$(date)]: "installed all the requirement"
 echo [$(date)]: "END"
+
+
